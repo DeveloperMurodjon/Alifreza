3b6cd8d1d416a604d46fd5c5bebf00fd00d813de
murodjon <murodjonisroiljonov4@gmail.com>
2024-11-17 23:56:45 +0500

    clone

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#068710",
+        lightdark: "",
+        dark: "#101017",
+        deepdark: "#050507",
+        lightgray: "#808080",
+        gray: "#313131",
+        deepgray: "#0A0A0F",
+      },
+    },
+  },
+  plugins: [],
+};
